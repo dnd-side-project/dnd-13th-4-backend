1c63806c38c9a2ebb4cef03d4414577abe1ba0ec
ohhamma <judyham10634@naver.com>
2025-08-11 17:57:52 +0900

    init: 스프링 프로젝트 초기 설정

---
+++ wini/src/main/java/com/example/wini/WiniApplication.java
@@ -0,0 +1,13 @@
+package com.example.wini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiniApplication.class, args);
+	}
+
+}
---
+++ wini/src/test/java/com/example/wini/WiniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
