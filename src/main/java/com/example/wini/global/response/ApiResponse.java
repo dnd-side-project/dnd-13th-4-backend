56d5d20d0c518cf60bb972f7d3fcbd77068928a3
duol9 <lhyy0195@gmail.com>
2025-08-16 17:47:42 +0900

    refactor: ApiResponse 성공 응답 메서드 개선

--- src/main/java/com/example/wini/global/response/ApiResponse.java
+++ src/main/java/com/example/wini/global/response/ApiResponse.java
@@ -1,6 +1,7 @@
 package com.example.wini.global.response;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 public class ApiResponse<T> {
@@ -15,12 +16,12 @@ public class ApiResponse<T> {
 
   // 상태 코드 200 OK를 기본값으로 사용하는 성공 응답
   public static <T> ApiResponse<T> success(final T data) {
-    return new ApiResponse<>(200, data);
+    return new ApiResponse<>(HttpStatus.OK.value(), data);
   }
 
   // 상태 코드 200 OK를 기본값으로 사용하는 성공 응답 (데이터 X)
   public static <T> ApiResponse<T> success() {
-    return new ApiResponse<>(200, null);
+    return new ApiResponse<>(HttpStatus.OK.value(), null);
   }
 
   // 데이터를 포함한 성공 응답
