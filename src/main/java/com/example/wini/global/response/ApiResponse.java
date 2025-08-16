a31c46a1df5e69fd8a161dff36eb1b8b181e3442
duol9 <lhyy0195@gmail.com>
2025-08-16 18:53:17 +0900

    refactor: ApiResponse를 record로 전환

--- src/main/java/com/example/wini/global/response/ApiResponse.java
+++ src/main/java/com/example/wini/global/response/ApiResponse.java
@@ -1,19 +1,8 @@
 package com.example.wini.global.response;
 
-import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
-@Getter
-public class ApiResponse<T> {
-
-  private final int status;
-  private final T data;
-
-  private ApiResponse(final int status, final T data) {
-    this.status = status;
-    this.data = data;
-  }
-
+public record ApiResponse<T>(int status, T data) {
   // 상태 코드 200 OK를 기본값으로 사용하는 성공 응답
   public static <T> ApiResponse<T> success(final T data) {
     return new ApiResponse<>(HttpStatus.OK.value(), data);
