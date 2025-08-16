d0c253c755c6691fd6f592ea1abe11d5b97a294c
duol9 <lhyy0195@gmail.com>
2025-08-16 17:37:34 +0900

    feat: 공동응답객체 추가

---
+++ src/main/java/com/example/wini/global/response/ApiResponse.java
@@ -0,0 +1,35 @@
+package com.example.wini.global.response;
+
+import lombok.Getter;
+
+@Getter
+public class ApiResponse<T> {
+
+  private final int status;
+  private final T data;
+
+  private ApiResponse(final int status, final T data) {
+    this.status = status;
+    this.data = data;
+  }
+
+  // 상태 코드 200 OK를 기본값으로 사용하는 성공 응답
+  public static <T> ApiResponse<T> success(final T data) {
+    return new ApiResponse<>(200, data);
+  }
+
+  // 상태 코드 200 OK를 기본값으로 사용하는 성공 응답 (데이터 X)
+  public static <T> ApiResponse<T> success() {
+    return new ApiResponse<>(200, null);
+  }
+
+  // 데이터를 포함한 성공 응답
+  public static <T> ApiResponse<T> success(final int status, final T data) {
+    return new ApiResponse<>(status, data);
+  }
+
+  // 데이터 없이 싱테 코드만 포함한 성공 응답
+  public static <T> ApiResponse<T> success(final int status) {
+    return new ApiResponse<>(status, null);
+  }
+}
