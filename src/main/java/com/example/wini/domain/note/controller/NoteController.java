78948a4938754df843cb982328c880a584e92066
ohhamma <judyham10634@naver.com>
2025-08-18 00:21:03 +0900

    feat: 마음쪽지 생성 API 구현

--- src/main/java/com/example/wini/domain/note/controller/NoteController.java
+++ src/main/java/com/example/wini/domain/note/controller/NoteController.java
@@ -1,17 +1,17 @@
 package com.example.wini.domain.note.controller;
 
+import com.example.wini.domain.note.dto.request.NoteCreateRequest;
 import com.example.wini.domain.note.dto.response.NoteResponse;
 import com.example.wini.domain.note.service.NoteService;
 import com.example.wini.global.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/notes")
@@ -40,5 +40,13 @@ public class NoteController {
   public ResponseEntity<ApiResponse<List<NoteResponse>>> getSavedNotes() {
     List<NoteResponse> responses = noteService.findSavedNotes();
     return ResponseEntity.ok(ApiResponse.success(responses));
+  }
+
+  @PostMapping
+  @Operation(summary = "쪽지 생성", description = "사용자가 쪽지를 생성합니다.")
+  public ResponseEntity<ApiResponse<NoteResponse>> createNote(
+      @Valid @ModelAttribute NoteCreateRequest request) {
+    NoteResponse response = noteService.createNote(request);
+    return ResponseEntity.ok(ApiResponse.success(HttpStatus.CREATED, response));
   }
 }
