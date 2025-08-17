af97f32d01b2f371441da44abc74ac4c7e80971e
ohhamma <judyham10634@naver.com>
2025-08-18 00:38:29 +0900

    feat: 최근과 오늘의 기준 분리하여 쪽지 필터링

--- src/main/java/com/example/wini/domain/note/repository/NoteCustomRepository.java
+++ src/main/java/com/example/wini/domain/note/repository/NoteCustomRepository.java
@@ -4,6 +4,8 @@ import com.example.wini.domain.note.domain.Note;
 import java.util.List;
 
 public interface NoteCustomRepository {
+  List<Note> findLatestNotes();
+
   List<Note> findTodayNotes();
 
   List<Note> findSavedNotes();
--- src/main/java/com/example/wini/domain/note/repository/NoteCustomRepositoryImpl.java
+++ src/main/java/com/example/wini/domain/note/repository/NoteCustomRepositoryImpl.java
@@ -5,6 +5,7 @@ import static com.example.wini.domain.note.domain.QNote.note;
 import com.example.wini.domain.note.domain.Note;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +14,11 @@ import lombok.RequiredArgsConstructor;
 public class NoteCustomRepositoryImpl implements NoteCustomRepository {
 
   private final JPAQueryFactory queryFactory;
+
+  @Override
+  public List<Note> findLatestNotes() {
+    return queryFactory.selectFrom(note).where(isLatest()).fetch();
+  }
 
   @Override
   public List<Note> findTodayNotes() {
@@ -24,7 +30,12 @@ public class NoteCustomRepositoryImpl implements NoteCustomRepository {
     return queryFactory.selectFrom(note).where(note.isSaved.eq(true)).fetch();
   }
 
-  private BooleanExpression isToday() {
+  private BooleanExpression isLatest() {
     return note.createdAt.after(LocalDateTime.now().minusHours(24));
+  }
+
+  private BooleanExpression isToday() {
+    return note.createdAt.between(
+        LocalDate.now().atStartOfDay(), LocalDate.now().plusDays(1).atStartOfDay());
   }
 }
