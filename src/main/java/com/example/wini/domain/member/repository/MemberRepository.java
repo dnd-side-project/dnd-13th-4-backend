87cfcf9a3043d88e610faa96dcfa2956437c2175
duol9 <lhyy0195@gmail.com>
2025-08-18 04:52:40 +0900

    feat: 회원, 상태 조회하는 메서드 구현
    - @EntityGraph로 status 즉시 로딩

---
+++ src/main/java/com/example/wini/domain/member/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package com.example.wini.domain.member.repository;
+
+import com.example.wini.domain.member.domain.Member;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+  @EntityGraph(attributePaths = "status")
+  Optional<Member> findWithStatusById(Long id);
+}
