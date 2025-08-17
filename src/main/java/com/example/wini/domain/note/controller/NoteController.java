53cf2262b9949b75412e6e7ab405146afafec32d
ohhamma <judyham10634@naver.com>
2025-08-18 00:40:23 +0900

    feat: 최근 받은 쪽지로 API 변경

--- src/main/java/com/example/wini/domain/note/controller/NoteController.java
+++ src/main/java/com/example/wini/domain/note/controller/NoteController.java
@@ -28,10 +28,10 @@ public class NoteController {
     return ResponseEntity.ok(ApiResponse.success(response));
   }
 
-  @GetMapping("/today")
-  @Operation(summary = "오늘 받은 쪽지 리스트 조회", description = "24시간 내 받은 쪽지 목록을 반환합니다.")
+  @GetMapping("/latest")
+  @Operation(summary = "최근 받은 쪽지 리스트 조회", description = "24시간 내 받은 쪽지 목록을 반환합니다.")
   public ResponseEntity<ApiResponse<List<NoteResponse>>> getTodayNotes() {
-    List<NoteResponse> responses = noteService.findTodayNotes();
+    List<NoteResponse> responses = noteService.findLatestNotes();
     return ResponseEntity.ok(ApiResponse.success(responses));
   }
 
