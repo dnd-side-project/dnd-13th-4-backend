2054b6f29aac52672a087376011caac920b76c4a
ohhamma <judyham10634@naver.com>
2025-08-17 23:49:18 +0900

    docs: swagger 문구 추가

--- src/main/java/com/example/wini/domain/note/controller/NoteController.java
+++ src/main/java/com/example/wini/domain/note/controller/NoteController.java
@@ -3,6 +3,8 @@ package com.example.wini.domain.note.controller;
 import com.example.wini.domain.note.dto.response.NoteResponse;
 import com.example.wini.domain.note.service.NoteService;
 import com.example.wini.global.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,24 +15,28 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/notes")
+@Tag(name = "2. 마음쪽지 관리", description = "마음쪽지 관련 API")
 @RequiredArgsConstructor
 public class NoteController {
 
   private final NoteService noteService;
 
   @GetMapping("/{noteId}")
+  @Operation(summary = "단일 쪽지 조회", description = "쪽지 내용을 반환합니다.")
   public ResponseEntity<ApiResponse<NoteResponse>> getNote(@PathVariable Long noteId) {
     NoteResponse response = noteService.findNoteById(noteId);
     return ResponseEntity.ok(ApiResponse.success(response));
   }
 
   @GetMapping("/today")
+  @Operation(summary = "오늘 받은 쪽지 리스트 조회", description = "24시간 내 받은 쪽지 목록을 반환합니다.")
   public ResponseEntity<ApiResponse<List<NoteResponse>>> getTodayNotes() {
     List<NoteResponse> responses = noteService.findTodayNotes();
     return ResponseEntity.ok(ApiResponse.success(responses));
   }
 
   @GetMapping("/saved")
+  @Operation(summary = "보관된 쪽지 리스트 조회", description = "사용자가 저장한 쪽지 목록을 반환합니다.")
   public ResponseEntity<ApiResponse<List<NoteResponse>>> getSavedNotes() {
     List<NoteResponse> responses = noteService.findSavedNotes();
     return ResponseEntity.ok(ApiResponse.success(responses));
