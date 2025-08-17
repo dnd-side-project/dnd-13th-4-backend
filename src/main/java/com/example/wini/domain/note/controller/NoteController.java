fab272de315a72bfd1bea571c908db18d62769db
ohhamma <judyham10634@naver.com>
2025-08-17 17:45:29 +0900

    feat: Note 단일 조회 api 연결

---
+++ src/main/java/com/example/wini/domain/note/controller/NoteController.java
@@ -0,0 +1,24 @@
+package com.example.wini.domain.note.controller;
+
+import com.example.wini.domain.note.dto.response.NoteResponse;
+import com.example.wini.domain.note.service.NoteService;
+import com.example.wini.global.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/notes")
+@RequiredArgsConstructor
+public class NoteController {
+
+  private final NoteService noteService;
+
+  @GetMapping("/{noteId}")
+  public ApiResponse<NoteResponse> getNote(@PathVariable Long noteId) {
+    NoteResponse response = noteService.findNoteById(noteId);
+    return ApiResponse.success(response);
+  }
+}
