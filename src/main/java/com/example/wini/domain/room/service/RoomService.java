a889e9dca141715ed9c83869d82a8b0833d53e49
duol9 <lhyy0195@gmail.com>
2025-08-20 04:44:36 +0900

    feat: 초대코드 및 방 생성 서비스 로직 구현

---
+++ src/main/java/com/example/wini/domain/room/service/RoomService.java
@@ -0,0 +1,65 @@
+package com.example.wini.domain.room.service;
+
+import static com.example.wini.global.error.exception.ErrorCode.MEMBER_NOT_FOUND;
+
+import com.example.wini.domain.member.domain.Member;
+import com.example.wini.domain.member.repository.MemberRepository;
+import com.example.wini.domain.room.dto.response.RoomResponse;
+import com.example.wini.domain.room.entity.MemberRoom;
+import com.example.wini.domain.room.entity.Room;
+import com.example.wini.domain.room.repository.MemberRoomRepository;
+import com.example.wini.domain.room.repository.RoomRepository;
+import com.example.wini.global.error.exception.CustomException;
+import java.security.SecureRandom;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class RoomService {
+
+  private static final Integer ROOM_CODE_LENGTH = 7;
+
+  private final MemberRepository memberRepository;
+  private final RoomRepository roomRepository;
+  private final MemberRoomRepository memberRoomRepository;
+
+  @Transactional
+  public RoomResponse createRoom(Long memberId) {
+    Member member =
+        memberRepository
+            .findById(memberId)
+            .orElseThrow(() -> new CustomException(MEMBER_NOT_FOUND));
+
+    String roomCode = generateUniqueRoomCode();
+
+    Room room = Room.create(roomCode);
+    Room saveRoom = roomRepository.save(room);
+
+    MemberRoom memberRoom = MemberRoom.create(member, room);
+    memberRoomRepository.save(memberRoom);
+
+    return RoomResponse.from(saveRoom);
+  }
+
+  private String generateUniqueRoomCode() {
+    String roomCode;
+    do {
+      roomCode = generateRoomCode();
+    } while (roomRepository.existsByRoomCode(roomCode));
+    return roomCode;
+  }
+
+  private String generateRoomCode() {
+    String charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    SecureRandom random = new SecureRandom();
+    StringBuilder codeBuilder = new StringBuilder(ROOM_CODE_LENGTH);
+
+    for (int i = 0; i < ROOM_CODE_LENGTH; i++) {
+      int index = random.nextInt(charSet.length());
+      codeBuilder.append(charSet.charAt(index));
+    }
+    return codeBuilder.toString();
+  }
+}
