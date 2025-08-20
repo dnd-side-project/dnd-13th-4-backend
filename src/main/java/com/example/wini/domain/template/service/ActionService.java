1918f6f16e91eec1805918a7c1e1c6ffbaa72997
ohhamma <judyham10634@naver.com>
2025-08-20 16:26:38 +0900

    feat: 행동 서비스 로직 구현

---
+++ src/main/java/com/example/wini/domain/template/service/ActionService.java
@@ -0,0 +1,24 @@
+package com.example.wini.domain.template.service;
+
+import com.example.wini.domain.template.domain.Action;
+import com.example.wini.domain.template.dto.response.ActionResponse;
+import com.example.wini.domain.template.repository.ActionRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ActionService {
+
+  private final ActionRepository actionRepository;
+
+  @Transactional(readOnly = true)
+  public List<ActionResponse> findAllActions() {
+    List<Action> actions = actionRepository.findAll();
+    return actions.stream().map(ActionResponse::from).toList();
+  }
+}
