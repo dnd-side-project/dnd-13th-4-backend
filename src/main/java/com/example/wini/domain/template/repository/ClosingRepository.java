31a12c64a1b80ba3fdd8ace1a4c4973a6f6de7ea
ohhamma <judyham10634@naver.com>
2025-08-20 19:40:19 +0900

    feat: 끝맺음 레포지토리 생성

---
+++ src/main/java/com/example/wini/domain/template/repository/ClosingRepository.java
@@ -0,0 +1,6 @@
+package com.example.wini.domain.template.repository;
+
+import com.example.wini.domain.template.domain.Closing;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClosingRepository extends JpaRepository<Closing, Long> {}
