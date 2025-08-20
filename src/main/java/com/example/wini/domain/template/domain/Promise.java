8eae5ba40da25781c2ce198d89eb960633a8748a
ohhamma <judyham10634@naver.com>
2025-08-20 18:08:00 +0900

    feat: 약속 엔티티 생성

---
+++ src/main/java/com/example/wini/domain/template/domain/Promise.java
@@ -0,0 +1,35 @@
+package com.example.wini.domain.template.domain;
+
+import com.example.wini.domain.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Promise extends BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(length = 10, nullable = false)
+  @Enumerated(EnumType.STRING)
+  private EmotionType emotionType;
+
+  @Column(length = 30, nullable = false)
+  private String text;
+
+  @Builder(access = AccessLevel.PRIVATE)
+  private Promise(EmotionType emotionType, String text) {
+    this.emotionType = emotionType;
+    this.text = text;
+  }
+
+  public static Promise create(EmotionType emotionType, String text) {
+    return Promise.builder().emotionType(emotionType).text(text).build();
+  }
+}
