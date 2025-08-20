c50ecb15434d944b0befe4adb77fc3330cb92c9a
ohhamma <judyham10634@naver.com>
2025-08-20 16:45:36 +0900

    fix: 변경된 엔티티 관계에 맞게 dto 변경

--- src/main/java/com/example/wini/domain/template/dto/response/ActionCategoryResponse.java
+++ src/main/java/com/example/wini/domain/template/dto/response/ActionCategoryResponse.java
@@ -1,10 +1,14 @@
 package com.example.wini.domain.template.dto.response;
 
 import com.example.wini.domain.template.domain.ActionCategory;
+import java.util.List;
 
-public record ActionCategoryResponse(Long id, String emotionType, String name) {
+public record ActionCategoryResponse(
+    Long id, String emotionType, String name, List<ActionResponse> actions) {
   public static ActionCategoryResponse from(ActionCategory category) {
+    List<ActionResponse> actions =
+        category.getActions().stream().map(ActionResponse::from).toList();
     return new ActionCategoryResponse(
-        category.getId(), category.getEmotionType().getValue(), category.getName());
+        category.getId(), category.getEmotionType().getValue(), category.getName(), actions);
   }
 }
--- src/main/java/com/example/wini/domain/template/dto/response/ActionResponse.java
+++ src/main/java/com/example/wini/domain/template/dto/response/ActionResponse.java
@@ -2,9 +2,8 @@ package com.example.wini.domain.template.dto.response;
 
 import com.example.wini.domain.template.domain.Action;
 
-public record ActionResponse(Long id, String text, ActionCategoryResponse category) {
+public record ActionResponse(Long id, String text) {
   public static ActionResponse from(Action action) {
-    return new ActionResponse(
-        action.getId(), action.getText(), ActionCategoryResponse.from(action.getCategory()));
+    return new ActionResponse(action.getId(), action.getText());
   }
 }
