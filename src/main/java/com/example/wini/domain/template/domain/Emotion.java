3f94dbc824120984ce8921b519e63667664cac7b
ohhamma <judyham10634@naver.com>
2025-08-20 15:05:09 +0900

    feat: 감정 엔티티 작성

---
+++ src/main/java/com/example/wini/domain/template/domain/Emotion.java
@@ -0,0 +1,38 @@
+package com.example.wini.domain.template.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Emotion {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(length = 10, nullable = false)
+  @Enumerated(EnumType.STRING)
+  private EmotionType emotionType;
+
+  @Column(length = 10, nullable = false)
+  private String text;
+
+  @Column(nullable = false)
+  private String graphicUrl;
+
+  @Builder(access = AccessLevel.PRIVATE)
+  private Emotion(EmotionType emotionType, String text, String graphicUrl) {
+    this.emotionType = emotionType;
+    this.text = text;
+    this.graphicUrl = graphicUrl;
+  }
+
+  public static Emotion create(EmotionType emotionType, String text, String graphicUrl) {
+    return Emotion.builder().emotionType(emotionType).text(text).graphicUrl(graphicUrl).build();
+  }
+}
