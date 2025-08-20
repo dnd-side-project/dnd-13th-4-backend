f62064e44916fddf8f682c3a408aa31f15a432fa
duol9 <lhyy0195@gmail.com>
2025-08-21 00:10:05 +0900

    chore: JPA Auditing 설정

---
+++ src/main/java/com/example/wini/global/config/JpaAuditingConfig.java
@@ -0,0 +1,18 @@
+package com.example.wini.global.config;
+
+import java.util.Optional;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {
+
+  @Bean
+  public AuditorAware<Long> auditorAware() {
+    // TODO: 인증인가 구현하면 여기도 바꾸기
+    return () -> Optional.of(1L);
+  }
+}
