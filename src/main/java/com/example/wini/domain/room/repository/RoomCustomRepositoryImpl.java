cbcedfa4432a8356ad08ccef406c94ed726c922f
duol9 <lhyy0195@gmail.com>
2025-08-20 23:39:23 +0900

    feat: 방 참여 전 검증 메서드 구현

--- src/main/java/com/example/wini/domain/room/repository/RoomCustomRepositoryImpl.java
+++ src/main/java/com/example/wini/domain/room/repository/RoomCustomRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.example.wini.domain.room.repository;
 import static com.example.wini.domain.room.entity.QMemberRoom.memberRoom;
 import static com.example.wini.domain.room.entity.QRoom.room;
 
+import com.example.wini.domain.room.entity.Room;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
@@ -15,11 +16,32 @@ public class RoomCustomRepositoryImpl implements RoomCustomRepository {
     @Override
     public Optional<Long> findOpenRoomIdByMemberId(Long memberId) {
         return Optional.ofNullable(queryFactory
-                .select(room.id)
+            .select(room.id)
+            .from(room)
+            .join(memberRoom)
+            .on(memberRoom.room.id.eq(room.id))
+            .where(memberRoom.member.id.eq(memberId).and(room.isClosed.isFalse()))
+            .fetchOne());
+    }
+
+      @Override
+      public Optional<Room> findOpenRoomByRoomCode(String roomCode) {
+        return Optional.ofNullable(
+            queryFactory
+                .selectFrom(room)
+                .where(room.roomCode.eq(roomCode).and(room.isClosed.isFalse()))
+                .fetchOne());
+      }
+
+      @Override
+      public boolean existsOpenRoomByMemberId(Long memberId) {
+        return queryFactory
+                .selectOne()
                 .from(room)
                 .join(memberRoom)
                 .on(memberRoom.room.id.eq(room.id))
                 .where(memberRoom.member.id.eq(memberId).and(room.isClosed.isFalse()))
-                .fetchOne());
-    }
+                .fetchFirst()
+            != null;
+      }
 }
