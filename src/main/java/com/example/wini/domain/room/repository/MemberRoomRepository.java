9a532b23ee833374d408c7947429934e660d1dd8
duol9 <lhyy0195@gmail.com>
2025-08-20 23:44:17 +0900

    feat: 방 인원 수 카운트 메서드 구현

--- src/main/java/com/example/wini/domain/room/repository/MemberRoomRepository.java
+++ src/main/java/com/example/wini/domain/room/repository/MemberRoomRepository.java
@@ -3,4 +3,7 @@ package com.example.wini.domain.room.repository;
 import com.example.wini.domain.room.entity.MemberRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRoomRepository extends JpaRepository<MemberRoom, Long> {}
+public interface MemberRoomRepository extends JpaRepository<MemberRoom, Long> {
+
+  Long countMembersByRoomId(Long roomId);
+}
