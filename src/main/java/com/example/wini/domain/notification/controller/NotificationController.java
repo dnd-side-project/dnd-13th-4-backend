1aa32cadabc871175a667fb62ebc1572fabb7db9
duol9 <lhyy0195@gmail.com>
2025-08-23 02:01:26 +0900

    feat: Notification 토큰 저장 API 추가

---
+++ src/main/java/com/example/wini/domain/notification/controller/NotificationController.java
@@ -0,0 +1,34 @@
+package com.example.wini.domain.notification.controller;
+
+import com.example.wini.domain.notification.dto.request.NotificationCreateRequest;
+import com.example.wini.domain.notification.service.NotificationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/notifications")
+@Tag(name = "7. 알림 관리", description = "알림 관련 API")
+@RequiredArgsConstructor
+public class NotificationController {
+
+    private static final long MEMBER_ID = 1L;
+
+    private final NotificationService notificationService;
+
+    @PostMapping("/tokens")
+    @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "알림 토큰 저장", description = "FCM 토큰을 저장합니다.")
+    public void saveNotificationToken(
+            // TODO: 사용자 정보
+            @Valid @RequestBody NotificationCreateRequest request) {
+        notificationService.saveNotificationToken(MEMBER_ID, request);
+    }
+}
