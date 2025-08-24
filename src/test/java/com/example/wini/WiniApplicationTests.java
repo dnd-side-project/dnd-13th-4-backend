5a8140e8e85e64a7c8f01b06e4533f4a149e972e
duol9 <lhyy0195@gmail.com>
2025-08-25 05:58:49 +0900

    test: 테스트용 가짜 알림 발송 메서드 추가

--- src/test/java/com/example/wini/WiniApplicationTests.java
+++ src/test/java/com/example/wini/WiniApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.wini;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class WiniApplicationTests {
 
     @Test
