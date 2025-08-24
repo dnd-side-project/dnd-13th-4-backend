56d771b30c10728a572ff2d5c3939390e76b4cca
duol9 <lhyy0195@gmail.com>
2025-08-25 00:45:14 +0900

    refactor: Firebase 키 주입 방식을 환경 변수로 변경
    - 기존의 파일 경로(`file-path`) 방식은 로컬(classpath:)과 서버(file:)의 경로를 다르게 설정했어야 했는데, 주입 방식을 환경변수로 통일해 동일한 환경이 되도록 변경

--- src/main/java/com/example/wini/global/config/FirebaseConfig.java
+++ src/main/java/com/example/wini/global/config/FirebaseConfig.java
@@ -9,23 +9,23 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
 import jakarta.annotation.PostConstruct;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
 
 @Configuration
 public class FirebaseConfig {
 
-    @Value("${fcm.file-path}")
-    private Resource firebaseResource;
+    @Value("${fcm.secret-key}")
+    private String fcmSecretKey;
 
     @PostConstruct
     public void init() {
         try {
-            InputStream keyFileStream = firebaseResource.getInputStream();
+            InputStream keyFileStream = new ByteArrayInputStream(fcmSecretKey.getBytes());
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(keyFileStream))
                     .build();
