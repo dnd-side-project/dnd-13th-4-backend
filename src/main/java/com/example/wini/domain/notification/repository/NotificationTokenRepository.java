57cb25b7bf6b3f87dc8c01880a668d331a1ed1fb
duol9 <lhyy0195@gmail.com>
2025-08-24 20:24:51 +0900

    feat: 알림토큰 조회 메서드 추가

--- src/main/java/com/example/wini/domain/notification/repository/NotificationTokenRepository.java
+++ src/main/java/com/example/wini/domain/notification/repository/NotificationTokenRepository.java
@@ -1,8 +1,11 @@
 package com.example.wini.domain.notification.repository;
 
 import com.example.wini.domain.notification.domain.NotificationToken;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationTokenRepository extends JpaRepository<NotificationToken, Long> {
     void deleteByToken(String token);
+
+    List<NotificationToken> findAllByMember_Id(Long memberId);
 }
