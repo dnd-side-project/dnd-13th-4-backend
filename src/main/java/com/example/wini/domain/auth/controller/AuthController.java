29efbd58f3c6ce337d9205116fec52ae4d673ef7
duol9 <lhyy0195@gmail.com>
2025-08-27 06:24:55 +0900

    feat: Swagger 문서에 Bearer 보안 스키마 적용
    - 로그인 등 시큐리티 필터가 필요하지 않는 곳엔 @SecurityRequirements(value = {})를 적용해 보안 요구사항 무시

--- src/main/java/com/example/wini/domain/auth/controller/AuthController.java
+++ src/main/java/com/example/wini/domain/auth/controller/AuthController.java
@@ -8,6 +8,7 @@ import com.example.wini.domain.auth.service.TokenService;
 import com.example.wini.domain.common.annotation.Auth;
 import com.example.wini.global.security.AuthMember;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirements;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,12 +28,14 @@ public class AuthController {
     private final TokenService tokenService;
 
     @GetMapping("/login/kakao")
+    @SecurityRequirements(value = {})
     @Operation(summary = "카카오 로그인 페이지", description = "카카오 로그인 페이지 주소를 반환합니다.")
     public String kakaoLoginForm() {
         return authService.getKakaoLoginForm();
     }
 
     @GetMapping("/kakao/callback")
+    @SecurityRequirements(value = {})
     @Operation(summary = "카카오 로그인", description = "토큰을 반환합니다.")
     public TokenResponse kakaoLogin(@RequestParam("code") String authCode) {
         return authService.kakaoLogin(authCode);
