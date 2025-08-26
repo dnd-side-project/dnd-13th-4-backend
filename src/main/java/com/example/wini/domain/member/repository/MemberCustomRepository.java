f80f9b37a4a70e5bdbdc2197cc24cf2862fd1083
duol9 <lhyy0195@gmail.com>
2025-08-27 03:00:27 +0900

    feat: 룸메이트 정보 조회 메서드 구현

--- src/main/java/com/example/wini/domain/member/repository/MemberCustomRepository.java
+++ src/main/java/com/example/wini/domain/member/repository/MemberCustomRepository.java
@@ -1,6 +1,7 @@
 package com.example.wini.domain.member.repository;
 
 import com.example.wini.domain.member.domain.Member;
+import com.example.wini.domain.member.dto.query.MateInfoQuery;
 import java.util.Optional;
 
 public interface MemberCustomRepository {
@@ -9,4 +10,6 @@ public interface MemberCustomRepository {
     Optional<Member> findRoommateWithStatusByMemberId(Long memberId);
 
     Optional<Member> findRoommateByMemberId(Long memberId);
+
+    Optional<MateInfoQuery> findRoommateWithJoinedAtByMemberId(Long memberId);
 }
