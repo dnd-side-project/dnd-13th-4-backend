54a0b24510ae02fcd15857bc06c5caf3dc5bbc3f
duol9 <lhyy0195@gmail.com>
2025-08-26 23:02:12 +0900

    feat: OauthId로 Member 조회하는 메서드 추가

--- src/main/java/com/example/wini/domain/member/repository/MemberRepository.java
+++ src/main/java/com/example/wini/domain/member/repository/MemberRepository.java
@@ -1,6 +1,10 @@
 package com.example.wini.domain.member.repository;
 
 import com.example.wini.domain.member.domain.Member;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Long>, MemberCustomRepository {}
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberCustomRepository {
+
+    Optional<Member> findByOauthId(String oauthId);
+}
