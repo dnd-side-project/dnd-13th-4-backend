c15ef546819eda81e56ceed67d7d4dea43aa4fea
duol9 <lhyy0195@gmail.com>
2025-08-27 04:39:08 +0900

    refactor: 당부 텍스트 길이 변경

--- src/main/java/com/example/wini/domain/template/domain/Promise.java
+++ src/main/java/com/example/wini/domain/template/domain/Promise.java
@@ -20,7 +20,7 @@ public class Promise extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private EmotionType emotionType;
 
-    @Column(length = 30, nullable = false)
+    @Column(length = 50, nullable = false)
     private String text;
 
     @Builder(access = AccessLevel.PRIVATE)
