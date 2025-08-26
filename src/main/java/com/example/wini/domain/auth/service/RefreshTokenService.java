ca6841fc8a2f8b460f9dd6ef7cb0c2149f64432e
duol9 <lhyy0195@gmail.com>
2025-08-26 23:28:42 +0900

    feat: 토큰 재발급 서비스 로직 구현
    - 추가 변경 사항 : generateToken -> generateTokens 로 메서드명 변경

--- src/main/java/com/example/wini/domain/auth/service/RefreshTokenService.java
+++ src/main/java/com/example/wini/domain/auth/service/RefreshTokenService.java
@@ -1,9 +1,15 @@
 package com.example.wini.domain.auth.service;
 
+import static com.example.wini.global.common.constant.SecurityConstants.REFRESH_TOKEN;
+import static com.example.wini.global.error.exception.ErrorCode.INVALID_REFRESH_TOKEN;
+import static com.example.wini.global.error.exception.ErrorCode.MEMBER_NOT_FOUND;
+
 import com.example.wini.domain.auth.domain.RefreshToken;
 import com.example.wini.domain.auth.dto.response.TokenResponse;
 import com.example.wini.domain.auth.repository.RefreshTokenRepository;
 import com.example.wini.domain.member.domain.Member;
+import com.example.wini.domain.member.repository.MemberRepository;
+import com.example.wini.global.error.exception.CustomException;
 import com.example.wini.global.security.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,16 +19,51 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class RefreshTokenService {
 
+    private final MemberRepository memberRepository;
     private final RefreshTokenRepository refreshTokenRepository;
     private final JwtProvider jwtProvider;
 
     @Transactional
-    public TokenResponse generateToken(Member member) {
+    public TokenResponse generateTokens(Member member) {
+        String accessToken = jwtProvider.generateAccessToken(member);
+        String refreshToken = jwtProvider.generateRefreshToken(member);
+
+        RefreshToken newRefreshTokenEntity = RefreshToken.create(member, jwtProvider.substringToken(refreshToken));
+        refreshTokenRepository.save(newRefreshTokenEntity);
+
+        return TokenResponse.of(accessToken, refreshToken);
+    }
+
+    @Transactional
+    public TokenResponse reissueToken(String refreshTokenHeader) {
+        String refreshToken = validateRefreshToken(refreshTokenHeader);
+
+        return updateAndGenerateTokens(refreshToken);
+    }
+
+    private String validateRefreshToken(String refreshTokenHeader) {
+        String refreshToken = jwtProvider.substringToken(refreshTokenHeader);
+
+        if (!jwtProvider.validToken(refreshToken, REFRESH_TOKEN)) {
+            throw new CustomException(INVALID_REFRESH_TOKEN);
+        }
+        return refreshToken;
+    }
+
+    private TokenResponse updateAndGenerateTokens(String refreshToken) {
+        Long userId = jwtProvider.getMemberId(refreshToken, REFRESH_TOKEN);
+
+        Member member = memberRepository.findById(userId).orElseThrow(() -> new CustomException(MEMBER_NOT_FOUND));
+
+        RefreshToken storedRefreshToken = refreshTokenRepository
+                .findByToken(refreshToken)
+                .orElseThrow(() -> new CustomException(INVALID_REFRESH_TOKEN));
+
         String newAccessToken = jwtProvider.generateAccessToken(member);
         String newRefreshToken = jwtProvider.generateRefreshToken(member);
 
-        RefreshToken refreshToken = RefreshToken.create(member, jwtProvider.substringToken(newRefreshToken));
-        refreshTokenRepository.save(refreshToken);
+        storedRefreshToken.updateToken(newRefreshToken);
+        refreshTokenRepository.save(storedRefreshToken);
 
         return TokenResponse.of(newAccessToken, newRefreshToken);
     }
