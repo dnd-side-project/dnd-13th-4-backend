6fdde1dd4111384dad883815ba8e888768629832
duol9 <lhyy0195@gmail.com>
2025-08-27 02:08:14 +0900

    chore: 캐시 설정

---
+++ src/main/java/com/example/wini/global/config/CacheConfig.java
@@ -0,0 +1,21 @@
+package com.example.wini.global.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import java.util.concurrent.TimeUnit;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    public CaffeineCacheManager cacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+        cacheManager.setCaffeine(
+                Caffeine.newBuilder().expireAfterWrite(30, TimeUnit.MINUTES).maximumSize(10000));
+        return cacheManager;
+    }
+}
