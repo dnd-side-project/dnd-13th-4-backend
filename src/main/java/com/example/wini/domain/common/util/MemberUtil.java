2ecf2a7956ebec9b4b458f5bf73f278f7a6193a4
duol9 <lhyy0195@gmail.com>
2025-08-27 00:09:29 +0900

    feat: AuthMember로 Member 객체 조회하는 메서드 구현

---
+++ src/main/java/com/example/wini/domain/common/util/MemberUtil.java
@@ -0,0 +1,25 @@
+package com.example.wini.domain.common.util;
+
+import static com.example.wini.global.error.exception.ErrorCode.MEMBER_NOT_FOUND;
+
+import com.example.wini.domain.member.domain.Member;
+import com.example.wini.domain.member.repository.MemberRepository;
+import com.example.wini.global.error.exception.CustomException;
+import com.example.wini.global.security.AuthMember;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+public class MemberUtil {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional(readOnly = true)
+    public Member getMember(AuthMember authMember) {
+        return memberRepository
+                .findById(authMember.memberId())
+                .orElseThrow(() -> new CustomException(MEMBER_NOT_FOUND));
+    }
+}
