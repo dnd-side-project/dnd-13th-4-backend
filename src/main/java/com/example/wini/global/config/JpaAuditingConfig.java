574669e0a15454e760447d9d54b562fcd9b4461a
duol9 <lhyy0195@gmail.com>
2025-08-27 06:13:46 +0900

    feat: JpaAuditingConfig에 회원 정보 입력되도록 구현

--- src/main/java/com/example/wini/global/config/JpaAuditingConfig.java
+++ src/main/java/com/example/wini/global/config/JpaAuditingConfig.java
@@ -1,10 +1,13 @@
 package com.example.wini.global.config;
 
+import com.example.wini.global.security.AuthMember;
 import java.util.Optional;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @EnableJpaAuditing
 @Configuration
@@ -12,7 +15,21 @@ public class JpaAuditingConfig {
 
     @Bean
     public AuditorAware<Long> auditorAware() {
-        // TODO: 인증인가 구현하면 여기도 바꾸기
-        return () -> Optional.of(1L);
+        return () -> {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            if (authentication == null
+                    || !authentication.isAuthenticated()
+                    || authentication
+                            instanceof org.springframework.security.authentication.AnonymousAuthenticationToken) {
+                return Optional.empty();
+            }
+
+            Object principal = authentication.getPrincipal();
+            if (principal instanceof AuthMember authMember) {
+                return Optional.of(authMember.memberId());
+            }
+
+            return Optional.empty();
+        };
     }
 }
