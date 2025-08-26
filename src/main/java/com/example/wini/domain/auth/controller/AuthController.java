52169f9d26bd3ab0287e2ad01ff74dc82e7b2fb9
duol9 <lhyy0195@gmail.com>
2025-08-27 02:13:36 +0900

    feat: 로그아웃 API 추가

--- src/main/java/com/example/wini/domain/auth/controller/AuthController.java
+++ src/main/java/com/example/wini/domain/auth/controller/AuthController.java
@@ -1,11 +1,18 @@
 package com.example.wini.domain.auth.controller;
 
+import static com.example.wini.global.common.constant.SecurityConstants.BEARER_TOKEN_PREFIX;
+
 import com.example.wini.domain.auth.dto.response.TokenResponse;
 import com.example.wini.domain.auth.service.AuthService;
+import com.example.wini.domain.auth.service.TokenService;
+import com.example.wini.domain.common.annotation.Auth;
+import com.example.wini.global.security.AuthMember;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
+    private final TokenService tokenService;
 
     @GetMapping("/login/kakao")
     @Operation(summary = "카카오 로그인 페이지", description = "카카오 로그인 페이지 주소를 반환합니다.")
@@ -28,5 +36,12 @@ public class AuthController {
     @Operation(summary = "카카오 로그인", description = "토큰을 반환합니다.")
     public TokenResponse kakaoLogin(@RequestParam("code") String authCode) {
         return authService.kakaoLogin(authCode);
+    }
+
+    @PostMapping("logout")
+    @Operation(summary = "로그아웃", description = "토큰을 무효화시킵니다.")
+    public void logout(@RequestHeader("Authorization") String header, @Auth AuthMember authMember) {
+        String accessToken = header.substring(BEARER_TOKEN_PREFIX.length());
+        tokenService.deleteToken(accessToken, authMember);
     }
 }
