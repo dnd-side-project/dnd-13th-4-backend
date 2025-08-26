acb124f4ae6d6fb778de60a62c31f675fc867619
ohhamma <judyham10634@naver.com>
2025-08-27 04:06:17 +0900

    feat: 주차별 받은 긍정쪽지 개수 조회하는 서비스 로직 추가

--- src/main/java/com/example/wini/domain/log/dto/response/GrowthResponse.java
+++ src/main/java/com/example/wini/domain/log/dto/response/GrowthResponse.java
@@ -1,10 +1,18 @@
 package com.example.wini.domain.log.dto.response;
 
-public record GrowthResponse(SimpleActionChange increasedPositiveAction, SimpleActionChange decreasedNegativeAction) {
+import java.util.List;
+
+public record GrowthResponse(
+        SimpleActionChange increasedPositiveAction,
+        SimpleActionChange decreasedNegativeAction,
+        List<WeeklyNoteCount> weeklyPositiveNoteCounts) {
     public static GrowthResponse from(
-            ActionChange increasedPositiveActionChange, ActionChange decreasedNegativeActionChange) {
+            ActionChange increasedPositiveActionChange,
+            ActionChange decreasedNegativeActionChange,
+            List<WeeklyNoteCount> weeklyPositiveNoteCounts) {
         return new GrowthResponse(
                 SimpleActionChange.from(increasedPositiveActionChange),
-                SimpleActionChange.from(decreasedNegativeActionChange));
+                SimpleActionChange.from(decreasedNegativeActionChange),
+                weeklyPositiveNoteCounts);
     }
 }
