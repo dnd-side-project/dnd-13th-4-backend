f3e03df8c950ccf3a2efc4237bcae53b54497a4b
duol9 <lhyy0195@gmail.com>
2025-08-27 20:44:24 +0900

    refactor: 카카오 로그인api 리다이렉트 방식으로 변경
    - 기존: /login/kakao 호출 시 /kakao/callback으로 리다이렉트 되는 주소 반환 (httpStatus 200)
    - 현재: /login/kakao 호출 시 반환되는 주소를 통해 리다이렉트 (httpStatus 302)

--- src/main/java/com/example/wini/domain/auth/controller/AuthController.java
+++ src/main/java/com/example/wini/domain/auth/controller/AuthController.java
@@ -10,6 +10,8 @@ import com.example.wini.global.security.AuthMember;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirements;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,14 +31,15 @@ public class AuthController {
 
     @GetMapping("/login/kakao")
     @SecurityRequirements(value = {})
-    @Operation(summary = "카카오 로그인 페이지", description = "카카오 로그인 페이지 주소를 반환합니다.")
-    public String kakaoLoginForm() {
-        return authService.getKakaoLoginForm();
+    @Operation(summary = "카카오 로그인", description = "카카오 로그인 페이지로 리다이렉트 됩니다.")
+    public void kakaoLoginForm(HttpServletResponse response) throws IOException {
+        String redirectUrl = authService.getKakaoLoginForm();
+        response.sendRedirect(redirectUrl);
     }
 
     @GetMapping("/kakao/callback")
     @SecurityRequirements(value = {})
-    @Operation(summary = "카카오 로그인", description = "토큰을 반환합니다.")
+    @Operation(summary = "카카오 로그인 콜백", description = "카카오 로그인 후 토큰을 반환합니다.")
     public TokenResponse kakaoLogin(@RequestParam("code") String authCode) {
         return authService.kakaoLogin(authCode);
     }
