52b1ad20599e5cfb2be7e0f8beb949629fef157a
duol9 <lhyy0195@gmail.com>
2025-08-28 11:46:04 +0900

    docs: 룸메 정보 Dto 오타 수정

--- src/main/java/com/example/wini/domain/member/dto/response/MateResponse.java
+++ src/main/java/com/example/wini/domain/member/dto/response/MateResponse.java
@@ -3,7 +3,7 @@ package com.example.wini.domain.member.dto.response;
 import com.example.wini.domain.member.dto.query.MateInfoQuery;
 import java.time.LocalDateTime;
 
-public record MateResponse(Long id, String name, String image, LocalDateTime joindAt) {
+public record MateResponse(Long id, String name, String image, LocalDateTime joinedAt) {
     public static MateResponse from(MateInfoQuery query) {
         return new MateResponse(query.id(), query.name(), query.image(), query.joindAt());
     }
