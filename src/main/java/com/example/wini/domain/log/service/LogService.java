cc71904b886908de9cc577c5dcb2e9323a9a8b61
ohhamma <judyham10634@naver.com>
2025-08-29 16:33:37 +0900

    feat: 통계 서비스에 인증된 사용자 연결

--- src/main/java/com/example/wini/domain/log/service/LogService.java
+++ src/main/java/com/example/wini/domain/log/service/LogService.java
@@ -2,7 +2,9 @@ package com.example.wini.domain.log.service;
 
 import static com.example.wini.global.error.exception.ErrorCode.*;
 
+import com.example.wini.domain.common.util.MemberUtil;
 import com.example.wini.domain.log.dto.response.*;
+import com.example.wini.domain.member.domain.Member;
 import com.example.wini.domain.note.repository.NoteRepository;
 import com.example.wini.domain.room.entity.Room;
 import com.example.wini.domain.room.repository.RoomRepository;
@@ -22,32 +24,35 @@ public class LogService {
 
     private final NoteRepository noteRepository;
     private final RoomRepository roomRepository;
+    private final MemberUtil memberUtil;
 
     @Transactional(readOnly = true)
     public StatisticsResponse getWeeklyStatistics() {
-        // TODO : 인가받은 사용자의 노트로 필터링 필요
-        Long notesSentThisWeek = noteRepository.countNotesSentThisWeek(1L);
-        Long notesReceivedThisWeek = noteRepository.countNotesReceivedThisWeek(1L);
-        Room room = roomRepository.findOpenRoomByMemberId(1L).orElseThrow(() -> new CustomException(ROOM_NOT_FOUND));
+        Member me = memberUtil.getCurrentMember();
+        Long notesSentThisWeek = noteRepository.countNotesSentThisWeek(me.getId());
+        Long notesReceivedThisWeek = noteRepository.countNotesReceivedThisWeek(me.getId());
+        Room room = roomRepository
+                .findOpenRoomByMemberId(me.getId())
+                .orElseThrow(() -> new CustomException(ROOM_NOT_FOUND));
 
         return StatisticsResponse.of(notesSentThisWeek, notesReceivedThisWeek, room.getCreatedAt());
     }
 
     @Transactional(readOnly = true)
     public KeywordResponse getTopActionCategoriesInLast30Days() {
-        // TODO : 인가받은 사용자의 노트로 필터링 필요
-        ActionCategory positive = noteRepository.findTopActionCategoryInLast30Days(1L, EmotionType.POSITIVE);
-        ActionCategory negative = noteRepository.findTopActionCategoryInLast30Days(1L, EmotionType.NEGATIVE);
+        Member me = memberUtil.getCurrentMember();
+        ActionCategory positive = noteRepository.findTopActionCategoryInLast30Days(me.getId(), EmotionType.POSITIVE);
+        ActionCategory negative = noteRepository.findTopActionCategoryInLast30Days(me.getId(), EmotionType.NEGATIVE);
 
         return KeywordResponse.from(positive, negative);
     }
 
     @Transactional(readOnly = true)
     public GrowthResponse getActionTrendsAndWeeklyPositiveNoteCounts() {
-        // TODO : 인가받은 사용자의 노트로 필터링 필요
-        ActionChange increasedPositiveAction = noteRepository.findMostIncreasedPositiveActionChange(1L);
-        ActionChange decreasedNegativeAction = noteRepository.findMostDecreasedNegativeActionChange(1L);
-        List<WeeklyNoteCount> weeklyPositiveNoteCounts = noteRepository.getWeeklyPositiveNoteCounts(1L);
+        Member me = memberUtil.getCurrentMember();
+        ActionChange increasedPositiveAction = noteRepository.findMostIncreasedPositiveActionChange(me.getId());
+        ActionChange decreasedNegativeAction = noteRepository.findMostDecreasedNegativeActionChange(me.getId());
+        List<WeeklyNoteCount> weeklyPositiveNoteCounts = noteRepository.getWeeklyPositiveNoteCounts(me.getId());
 
         return GrowthResponse.from(increasedPositiveAction, decreasedNegativeAction, weeklyPositiveNoteCounts);
     }
