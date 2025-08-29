5e66d6540c299a707ed6028b271487f9e8c34214
ohhamma <judyham10634@naver.com>
2025-08-30 06:26:41 +0900

    fix: ActionChange null 들어오면 모든 필드 null값으로 채움

--- src/main/java/com/example/wini/domain/log/dto/response/SimpleActionChange.java
+++ src/main/java/com/example/wini/domain/log/dto/response/SimpleActionChange.java
@@ -2,6 +2,9 @@ package com.example.wini.domain.log.dto.response;
 
 public record SimpleActionChange(String text, Long monthlyChange) {
     public static SimpleActionChange from(ActionChange actionChange) {
+        if (actionChange == null) {
+            return new SimpleActionChange(null, null);
+        }
         return new SimpleActionChange(actionChange.action().getText(), actionChange.monthlyChange());
     }
 }
