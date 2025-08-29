03ba8ba0653a12df79b485c14013070ae1456b43
duol9 <lhyy0195@gmail.com>
2025-08-29 22:08:55 +0900

    feat: 상태 시간 관련 상수 작성

---
+++ src/main/java/com/example/wini/global/common/constant/StatusReserveTimeConstants.java
@@ -0,0 +1,13 @@
+package com.example.wini.global.common.constant;
+
+public class StatusReserveTimeConstants {
+
+    public static final int SECONDS_PER_HOUR = 3600;
+    public static final int SECONDS_PER_MINUTE = 60;
+
+    public static final long INDEFINITE_SECONDS = 365L * 100 * 24 * 60 * 60;
+    public static final long INDEFINITE_HOUR = -1L;
+    public static final long INDEFINITE_MINUTE = -1L;
+
+    private StatusReserveTimeConstants() {}
+}
