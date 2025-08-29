427e0f8f7cee1a5626bf4a66e12328b1a06cb3dc
ohhamma <judyham10634@naver.com>
2025-08-30 04:15:45 +0900

    fix: dto 필드 타입 Long으로 변경

--- src/main/java/com/example/wini/domain/log/dto/response/SimpleActionChange.java
+++ src/main/java/com/example/wini/domain/log/dto/response/SimpleActionChange.java
@@ -1,6 +1,6 @@
 package com.example.wini.domain.log.dto.response;
 
-public record SimpleActionChange(String text, long monthlyChange) {
+public record SimpleActionChange(String text, Long monthlyChange) {
     public static SimpleActionChange from(ActionChange actionChange) {
         return new SimpleActionChange(actionChange.action().getText(), actionChange.monthlyChange());
     }
