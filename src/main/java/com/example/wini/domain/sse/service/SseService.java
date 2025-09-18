cab0849e5b3aaa1ab9bdfb50ddfd59405855db6e
ohhamma <judyham10634@naver.com>
2025-09-18 19:38:00 +0900

    refactor: 하나라도 전송 실패시 계속 전송하도록 수정
    - 실패시 오류 로그 남기기

--- src/main/java/com/example/wini/domain/sse/service/SseService.java
+++ src/main/java/com/example/wini/domain/sse/service/SseService.java
@@ -24,20 +24,27 @@ public class SseService {
 
     public SseEmitter subscribe(String lastEventId) {
         Long memberId = memberUtil.getCurrentMemberId();
-        String id = memberId + "_" + System.currentTimeMillis();
+        String eventId = memberId + "_" + System.currentTimeMillis();
 
-        SseEmitter emitter = emitterRepository.save(id, new SseEmitter(DEFAULT_TIMEOUT));
+        SseEmitter emitter = emitterRepository.save(eventId, new SseEmitter(DEFAULT_TIMEOUT));
 
-        emitter.onCompletion(() -> emitterRepository.deleteById(id));
-        emitter.onTimeout(() -> emitterRepository.deleteById(id));
+        emitter.onCompletion(() -> {
+            log.info("[SseService] onCompletion callback");
+            emitterRepository.deleteById(eventId);
+        });
+        emitter.onTimeout(() -> {
+            log.info("[SseService] onTimeout callback");
+            emitter.complete();
+            emitterRepository.deleteById(eventId);
+        });
 
-        sendToClient(emitter, id, "EventStream Created. [memberId=" + memberId + "]");
+        sendToClient(eventId, emitter, "EventStream Created. [memberId=" + memberId + "]");
 
         if (!lastEventId.isEmpty()) {
             Map<String, Object> events = emitterRepository.findAllEventCacheStartWithMemberId(String.valueOf(memberId));
             events.entrySet().stream()
                     .filter(entry -> lastEventId.compareTo(entry.getKey()) < 0)
-                    .forEach(entry -> sendToClient(emitter, entry.getKey(), entry.getValue()));
+                    .forEach(entry -> sendToClient(entry.getKey(), emitter, entry.getValue()));
         }
 
         return emitter;
@@ -45,19 +52,25 @@ public class SseService {
 
     public void send(Member receiver, Object data) {
         String receiverId = String.valueOf(receiver.getId());
+        String eventId = receiverId + "_" + System.currentTimeMillis();
 
         Map<String, SseEmitter> sseEmitters = emitterRepository.findAllStartWithMemberId(receiverId);
         sseEmitters.forEach((key, emitter) -> {
-            emitterRepository.saveEventCache(key, data);
-            sendToClient(emitter, key, data);
+            try {
+                emitterRepository.saveEventCache(key, data);
+                sendToClient(eventId, emitter, data);
+            } catch (CustomException e) {
+                emitterRepository.deleteById(key);
+                log.info("[SseService] Failed to send SSE", e);
+            }
         });
     }
 
-    private void sendToClient(SseEmitter emitter, String id, Object data) {
+    private void sendToClient(String eventId, SseEmitter emitter, Object data) {
         try {
-            emitter.send(SseEmitter.event().id(id).name("sse").data(data));
-        } catch (IOException exception) {
-            emitterRepository.deleteById(id);
+            emitter.send(SseEmitter.event().id(eventId).name("sse").data(data));
+        } catch (IOException e) {
+            emitterRepository.deleteById(eventId);
             throw new CustomException(ErrorCode.SSE_CONNECTION_ERROR);
         }
     }
